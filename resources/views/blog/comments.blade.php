04b0d9d1e5a639afef246baac97d6ff0870a01aa
Walter Prorok <waltprorok@gmail.com>
2018-07-31 16:17:09 -0400

    created and migrated database

--- resources/views/blog/comments.blade.php
+++ resources/views/blog/comments.blade.php
@@ -1,8 +1,31 @@
 <article class="post-comments">
-        <h3><i class="fa fa-comments"></i> 5 Comments</h3>
+  <h3><i class="fa fa-comments"></i> 5 Comments</h3>
 
-        <div class="comment-body padding-10">
-          <ul class="comments-list">
+  <div class="comment-body padding-10">
+    <ul class="comments-list">
+      <li class="comment-item">
+        <div class="comment-heading clearfix">
+          <div class="comment-author-meta">
+            <h4>John Doe <small>January 14, 2018</small></h4>
+          </div>
+        </div>
+        <div class="comment-content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+          <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+        </div>
+      </li>
+
+      <li class="comment-item">
+        <div class="comment-heading clearfix">
+          <div class="comment-author-meta">
+            <h4>John Doe <small>January 14, 2018</small></h4>
+          </div>
+        </div>
+        <div class="comment-content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+          <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+          <ul class="comments-list-children">
             <li class="comment-item">
               <div class="comment-heading clearfix">
                 <div class="comment-author-meta">
@@ -37,71 +60,48 @@
                       <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
                     </div>
                   </li>
-
-                  <li class="comment-item">
-                    <div class="comment-heading clearfix">
-                      <div class="comment-author-meta">
-                        <h4>John Doe <small>January 14, 2018</small></h4>
-                      </div>
-                    </div>
-                    <div class="comment-content">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
-                      <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
-
-                      <ul class="comments-list-children">
-                        <li class="comment-item">
-                          <div class="comment-heading clearfix">
-                            <div class="comment-author-meta">
-                              <h4>John Doe <small>January 14, 2018</small></h4>
-                            </div>
-                          </div>
-                          <div class="comment-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
-                            <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
                 </ul>
               </div>
             </li>
           </ul>
-
         </div>
+      </li>
+    </ul>
 
-        <div class="comment-footer padding-10">
-          <h3>Leave a comment</h3>
-          <form>
-            <div class="form-group required">
-              <label for="name">Name</label>
-              <input name="name" id="name" class="form-control" type="text">
-            </div>
-            <div class="form-group required">
-              <label for="email">Email</label>
-              <input name="email" id="email" class="form-control" type="text">
-            </div>
-            <div class="form-group">
-              <label for="website">Website</label>
-              <input name="website" id="website" class="form-control" type="text">
-            </div>
-            <div class="form-group required">
-              <label for="comment">Comment</label>
-              <textarea name="comment" id="comment" rows="6" class="form-control"></textarea>
-            </div>
-            <div class="clearfix">
-              <div class="pull-left">
-                <button type="submit" class="btn btn-lg btn-success">Submit</button>
-              </div>
-              <div class="pull-right">
-                <p class="text-muted">
-                  <span class="required">*</span>
-                  <em>Indicates required fields</em>
-                </p>
-              </div>
-            </div>
-          </form>
+  </div>
+
+  <div class="comment-footer padding-10">
+    <h3>Leave a comment</h3>
+    <form>
+      <div class="form-group required">
+        <label for="name">Name</label>
+        <input name="name" id="name" class="form-control" type="text">
+      </div>
+      <div class="form-group required">
+        <label for="email">Email</label>
+        <input name="email" id="email" class="form-control" type="text">
+      </div>
+      <div class="form-group">
+        <label for="website">Website</label>
+        <input name="website" id="website" class="form-control" type="text">
+      </div>
+      <div class="form-group required">
+        <label for="comment">Comment</label>
+        <textarea name="comment" id="comment" rows="6" class="form-control"></textarea>
+      </div>
+      <div class="clearfix">
+        <div class="pull-left">
+          <button type="submit" class="btn btn-lg btn-success">Submit</button>
         </div>
+        <div class="pull-right">
+          <p class="text-muted">
+            <span class="required">*</span>
+            <em>Indicates required fields</em>
+          </p>
+        </div>
+      </div>
+    </form>
+  </div>
 
-      </article>
+</article>
 

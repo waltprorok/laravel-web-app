db410a3db6d2500362900bde9369612597b8f293
Walter Prorok <waltprorok@gmail.com>
2018-08-01 15:49:41 -0400

    Markdown support to the Blog post

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use GrahamCampbell\Markdown\Facades\Markdown;
+
 
 class Post extends Model
 {
@@ -36,6 +38,11 @@ class Post extends Model
     {
         return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
     }
+    
+    public function getBodyHtmlAttribute($value)
+    {
+        return $this->body ? Markdown::convertToHtml(e($this->body)) : null;
+    }
 
     public function scopeLatestFirst($query)
     {
@@ -45,6 +52,11 @@ class Post extends Model
     public function scopePublished($query)
     {
         return $query->where("published_at", "<=", Carbon::now());
+    }
+    
+    public function getExcerptHtmlAttribute($value)
+    {
+        return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : null;
     }
 
 }
